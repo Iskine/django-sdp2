6b6d74066a6441d1286aae24cb18e4e880e66ead
Iskine <nusara.toy@gmail.com>
2023-04-14 16:55:04 +0700

    second commit

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    path('signup', views.sign_up, name='sign_up'),
+    path('password_reset', views.password_reset_request, name='password_reset'),
+]
